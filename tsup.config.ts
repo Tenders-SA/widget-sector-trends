7486cd822ea692542967a1119fcf23bc5d393db6
USER <mobiusndou@gmail.com>
2026-05-27 20:28:18 +0200

    chore: rename, fix dts config, add publishConfig

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: { 'widget-sector-trends': 'src/index.ts' },
   format: ['esm', 'iife'],
-  dts: true,
+  dts: {
+    resolve: true,
+    compilerOptions: {
+      ignoreDeprecations: '6.0',
+    },
+  },
   sourcemap: true,
   clean: true,
   target: 'es2022',
